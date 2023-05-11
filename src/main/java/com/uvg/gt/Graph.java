30934bd50cf30f388c8f7af22a4be6ca96fbc78f
ElrohirGT <elrohirgt@gmail.com>
2023-05-10 22:40:34 -0600

    feat: Added basic classes for implementing the interfaces

---
+++ src/main/java/com/uvg/gt/Graph.java
@@ -0,0 +1,37 @@
+package com.uvg.gt;
+
+import java.util.List;
+
+public class Graph implements IGraph {
+
+    @Override
+    public void addNode(Node node) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addNode'");
+    }
+
+    @Override
+    public Node removeNode(String node) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeNode'");
+    }
+
+    @Override
+    public void addRelation(String start, String end, int weight) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addRelation'");
+    }
+
+    @Override
+    public void removeRelation(String start, String end) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeRelation'");
+    }
+
+    @Override
+    public List<Node> getNodes() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNodes'");
+    }
+
+}
---
+++ src/main/java/com/uvg/gt/PathFinder.java
@@ -0,0 +1,19 @@
+package com.uvg.gt;
+
+import java.util.List;
+
+public class PathFinder implements IPathFinder {
+
+    @Override
+    public void updateShortestPath() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateShortestPath'");
+    }
+
+    @Override
+    public List<Node> constructPath(String StartNode, String EndNode) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'constructPath'");
+    }
+
+}
