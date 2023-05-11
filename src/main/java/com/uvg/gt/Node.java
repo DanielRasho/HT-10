60affb9c9b9a227465cf4c81e456eb1d1fa28ca5
ElrohirGT <elrohirgt@gmail.com>
2023-05-10 22:39:24 -0600

    feat: Implemented the node class

--- src/main/java/com/uvg/gt/Node.java
+++ src/main/java/com/uvg/gt/Node.java
@@ -4,14 +4,14 @@ import java.util.HashMap;
 
 public class Node {
 
-    /** Used to identify easily within a graph.*/
+    /** Used to identify easily within a graph. */
     int id;
 
     String label;
 
     HashMap<Node, Integer> neighbors;
 
-    public Node(String label){
+    public Node(String label) {
         this.label = label;
     }
 
@@ -19,7 +19,31 @@ public class Node {
         this.id = id;
     }
 
-    public void addNeighbor (Node neighbor, int weight) {
+    public int getId() {
+        return id;
+    }
 
+    public void addNeighbor(Node neighbor, int weight) {
+        neighbors.put(neighbor, weight);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Node)) {
+            return false;
+        }
+
+        Node other = (Node) obj;
+        return id == other.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return label;
     }
 }
