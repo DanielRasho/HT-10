fe476af684e3b85b1ffd97b355731554da1bc520
ElrohirGT <elrohirgt@gmail.com>
2023-05-11 10:26:05 -0600

    feat: Added equals and hashcode methods for relationship

--- src/main/java/com/uvg/gt/Model/Relationship.java
+++ src/main/java/com/uvg/gt/Model/Relationship.java
@@ -33,4 +33,43 @@ public class Relationship {
     public void changeClimate(Climates newClimate) {
         currentClimate = newClimate;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((origin == null) ? 0 : origin.hashCode());
+        result = prime * result + ((destination == null) ? 0 : destination.hashCode());
+        result = prime * result + ((currentClimate == null) ? 0 : currentClimate.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Relationship other = (Relationship) obj;
+        if (origin == null) {
+            if (other.origin != null)
+                return false;
+        } else if (!origin.equals(other.origin))
+            return false;
+        if (destination == null) {
+            if (other.destination != null)
+                return false;
+        } else if (!destination.equals(other.destination))
+            return false;
+        if (currentClimate != other.currentClimate)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s -[%s]-> %s", origin, getWeight(), destination);
+    }
 }
