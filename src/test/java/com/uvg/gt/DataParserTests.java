add492e848c2ad58efe6c4fd4309a1399c565032
ElrohirGT <elrohirgt@gmail.com>
2023-05-11 08:54:12 -0600

    fix: Removed unused imports

--- src/test/java/com/uvg/gt/DataParserTests.java
+++ src/test/java/com/uvg/gt/DataParserTests.java
@@ -1,8 +1,6 @@
 package com.uvg.gt;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 import com.uvg.gt.Model.Climates;
