176faadf2f31b23e7f8e3188650550ae6c9fbe25
Daniel Rayo <ray22933@uvg.edu.gt>
2023-05-13 18:35:12 -0600

    Feat: Implement graph class, and its unit test.

--- src/main/java/com/uvg/gt/Graph.java
+++ src/main/java/com/uvg/gt/Graph.java
@@ -1,50 +1,131 @@
 package com.uvg.gt;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.uvg.gt.Model.Node;
 import com.uvg.gt.Model.Relationship;
 
 public class Graph implements IGraph {
 
-    public Graph(List<Relationship> relations) {
+    List<Node> nodes;
+    List<Relationship> relations;
+
+    public Graph(List<Relationship> relations){
+        this.relations = relations;
+        this.nodes = getNodes();
     }
 
     @Override
     public void addNode(Node node) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addNode'");
+        if (!nodeExist(node))
+            nodes.add(node);
     }
 
     @Override
-    public Node removeNode(String node) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeNode'");
-    }
-
-    @Override
-    public List<Node> getNodes() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getNodes'");
+    public Node removeNode(String nodeLabel) {
+        Node node = new Node(nodeLabel);
+        if (!nodeExist(nodeLabel))
+            throw new RuntimeException("Error: Cannot remove a node that does not exist within the graph.");
+        List<Relationship> nodeRelations = relations.stream()
+                        .filter(o -> o.getOrigin().equals(node)
+                        || o.getDestination().equals(node)).toList();
+        if (nodeRelations.size() != 0)
+            throw new RuntimeException(String.format("Error: Cannot remove %s node first remove its relations", node));
+        nodes.remove(node);
+        return node;
     }
 
     @Override
     public void addRelation(Relationship relation) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addRelation'");
+        if(existRelation(relation))
+            return;
+        addNode(relation.getOrigin());
+        addNode(relation.getDestination());
+        relations.add(relation);
     }
 
     @Override
-    public void removeRelation(Node start, Node end) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeRelation'");
+    public void removeRelation(Node origin, Node destination) {
+        Optional<Relationship> relation = getRelation(origin, destination);
+        relation.ifPresent(relationship -> relations.remove(relationship));
+    }
+
+    @Override
+    public List<Node> getNodes() {
+        List<Node> nodes;
+        Stream<Node> origins = this.relations.stream()
+                        .map(Relationship::getOrigin);
+        Stream<Node> destinations = this.relations.stream()
+                .map(Relationship::getDestination);
+        nodes = Stream.concat(origins, destinations)
+                .distinct()
+                .collect(Collectors.toList());
+        return nodes;
     }
 
     @Override
     public Optional<Relationship> getRelation(Node origin, Node destination) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRelation'");
+        return relations.stream().filter(o -> o.getOrigin().equals(origin))
+                                        .filter(d -> d.getDestination().equals(destination))
+                                        .findFirst();
     }
 
+    @Override
+    public Map<String, Map<String, Double>> getGraphAsMatrix(){
+
+        TreeMap<String, Map<String, Double>> matrix = new TreeMap<>();
+
+        // Selecting a node as origin
+        for(Node origin : nodes){
+            TreeMap<String, Double> subMatrix = new TreeMap<>();
+            // Setting relation weight between a node an the rest.
+            for (Node destination : nodes){
+                Double weight;
+                // The weight of node to itself must be 0;
+                if (origin.equals(destination))
+                    weight = 0.0;
+                // Else the weight can be POSITIVE INFINITY (There's no relation) or a DOUBLE (There's relation)
+                else {
+                    Optional<Relationship> relation = getRelation(origin, destination);
+                    weight = relation.map(relationship -> (double) relationship.getWeight())
+                            .orElse(Double.POSITIVE_INFINITY);
+                }
+
+                // Adding relation
+                subMatrix.put(destination.getLabel(), weight);
+            }
+            // Add all relations
+            matrix.put(origin.getLabel(), subMatrix);
+        }
+
+        return matrix;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder message = new StringBuilder();
+        message.append("Nodes: \n");
+        for (Node node: nodes) {
+            message.append(node.toString() + "\n");
+        }
+        message.append("Relations: \n");
+        for (Relationship relation: relations) {
+            message.append(relation.toString() + "\n");
+        }
+        return message.toString();
+    }
+
+    public boolean nodeExist(Node node){
+        return nodes.contains(node);
+    }
+
+    public boolean nodeExist(String nodeLabel){
+        return nodes.contains(new Node(nodeLabel));
+    }
+
+    public boolean existRelation(Relationship relation){
+        return relations.contains(relation);
+    }
 }
---
+++ src/test/java/com/uvg/gt/GraphTest.java
@@ -0,0 +1,97 @@
+package com.uvg.gt;
+
+import com.uvg.gt.Model.DataParser;
+import com.uvg.gt.Model.Node;
+import com.uvg.gt.Model.Relationship;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class GraphTest {
+
+    static Graph graph;
+    static DataParser parser = new DataParser();
+
+    @BeforeAll
+    static void setUp() {
+        List<Relationship> relations = new ArrayList<>();
+        relations.add(parser.parse("BuenosAires SaoPaulo 10 15 20 50"));
+        relations.add(parser.parse("BuenosAires Lima 15 20 30 70"));
+        relations.add(parser.parse("Lima Quito 10 12 15 20"));
+        graph = new Graph(relations);
+    }
+
+    @Test
+    @Order(1)
+     void addNode() {
+        Node node = new Node("NewYork");
+        graph.addNode(node);
+        System.out.println(graph.toString());
+        assertTrue(graph.nodeExist(node));
+    }
+
+    @Test
+    @Order(2)
+    void addRelation() {
+        Relationship relationship = parser.parse("NewYork SaoPaulo 10 15 20 50");
+        graph.addRelation(relationship);
+        System.out.println(graph.toString());
+    }
+
+    @Test
+    @Order(3)
+    void removeRelation() {
+        Relationship relationship = parser.parse("NewYork SaoPaulo 10 15 20 50");
+        Node origin = new Node("NewYork");
+        Node destination = new Node("SaoPaulo");
+        graph.removeRelation(origin, destination);
+        System.out.println(graph.toString());
+    }
+
+    @Test
+    @Order(4)
+    void removeNode() {
+        Node node = new Node("NewYork");
+        graph.removeNode(node.getLabel());
+        System.out.println(graph.toString());
+        assertFalse(graph.nodeExist(node));
+    }
+
+    @Test
+    @Order(5)
+    void getNodes(){
+        List<Node> nodes = new ArrayList<>();
+        nodes.add(new Node("BuenosAires"));
+        nodes.add(new Node ("Lima"));
+        nodes.add(new Node ("SaoPaulo"));
+        nodes.add(new Node ("Quito"));
+        assertEquals(nodes, graph.getNodes());
+    }
+    @Test
+    @Order(6)
+    void getRelation(){
+        Node origin = new Node("Lima");
+        Node destination = new Node("Quito");
+        int relationWeight = graph.getRelation(origin, destination).get().getWeight();
+        assertEquals(10, relationWeight);
+    }
+
+    @Test
+    @Order(7)
+    void getUndefinedRelation(){
+        Node origin = new Node("Lima");
+        Node destination = new Node("Harry Potter");
+        boolean existRelation = graph.getRelation(origin, destination).isEmpty();
+        assertTrue(existRelation);
+    }
+
+    @Test
+    @Order(8)
+    void getGraphAsMatrix(){
+        System.out.println(graph.getGraphAsMatrix().toString());
+    }
+}
