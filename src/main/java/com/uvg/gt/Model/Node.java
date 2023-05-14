62bb33c9ac0b95f429d38d96723cf78115bdc8a2
Daniel Rayo <ray22933@uvg.edu.gt>
2023-05-13 18:33:31 -0600

    Fix: Removing node unnecessary fields and methods.

--- src/main/java/com/uvg/gt/Model/Node.java
+++ src/main/java/com/uvg/gt/Model/Node.java
@@ -4,31 +4,14 @@ import java.util.HashMap;
 
 public class Node {
 
-    /** Used to identify easily within a graph. */
-    int id;
-
     String label;
-
-    HashMap<Node, Integer> neighbors;
 
     public Node(String label) {
         this.label = label;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
     public String getLabel() {
         return label;
-    }
-
-    public void addNeighbor(Node neighbor, int weight) {
-        neighbors.put(neighbor, weight);
     }
 
     @Override
@@ -38,7 +21,7 @@ public class Node {
         }
 
         Node other = (Node) obj;
-        return id == other.getId() && label.equals(other.getLabel());
+        return label.equals(other.getLabel());
     }
 
     @Override
